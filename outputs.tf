e6f2b47479e51c3465b7b30449f72f807ac38d95
PGS-SOFT\bpach <bpach@pgs-soft.com>
2018-07-16 14:17:26 +0200

    Added vn resource

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "out_az_vn" {
+  value = "${azurerm_virtual_network.vnTerraform.address_space}"
+}
--- resources.tf
+++ resources.tf
@@ -1,1 +1,26 @@
+resource "azurerm_resource_group" "az_vn" {
+  name     = "vn_terraform"
+  location = "westus"
+}
 
+resource "azurerm_virtual_network" "vnTerraform" {
+  address_space       = ["10.0.0.0/16"]
+  location            = "West Europe"
+  name                = "vnTerraform"
+  resource_group_name = "${azurerm_resource_group.az_vn.name}"
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags {
+    environment = "terraformdev"
+  }
+}
